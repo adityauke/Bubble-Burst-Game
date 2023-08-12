de5163a0d8e0139b0e5675944a267dab0aace61d
Aditya Uke <adityauke4141@gmail.com>
2023-08-12 15:31:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function createBubble(){
+    let bubble = "";
+
+    for(let i = 0 ; i<=120 ; i++){
+        let rn = Math.floor(Math.random()*10)
+        bubble += `<div class="bubble">${rn}</div>`
+    }
+
+    let row = document.querySelector("#pbtm")
+    console.log(bubble);
+    row.innerHTML = bubble;
+}
+
+let timeval = 60;
+function runTimer(){
+    let timer = setInterval( function() {
+        if(timeval > 0){
+            timeval--;
+            document.querySelector('#timeval').textContent = timeval;
+        }
+        else{
+            clearInterval(timer);
+            document.querySelector("#pbtm").innerHTML=`<h1>Game Over... Your Score is ${score}!🎯</h1>`;
+        }
+        
+    },1000)
+}
+
+let hitrn;
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+let score = 0;
+function updateScore(){
+    score+=10;
+    document.querySelector("#scoreval").textContent = score
+}
+
+document.querySelector("#pbtm").addEventListener("click" , function(dets){
+    let currentBubble =Number(dets.target.textContent);
+    if(currentBubble === hitrn){
+        updateScore();
+        createBubble();
+        getNewHit();
+    }
+});
+
+
+createBubble();
+runTimer();
+getNewHit();
+
